2c1a4248e55f31fa654803d7c028eac39ca8abe6
Zync Team <info@zyncrender.com>
2017-02-13 11:29:55 -0800

    New code release.

--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ import zync_lib.appdirs.appdirs as appdirs
 
 class Settings(object):
   APP_NAME = 'Zync'
+  APP_AUTHOR = 'Google'
   settings = None
 
   def __init__(self):
@@ -32,7 +33,7 @@ class Settings(object):
 
   @staticmethod
   def _read():
-    config_dir = appdirs.user_data_dir(Settings.APP_NAME)
+    config_dir = Settings._alloc_config_dir()
     config_filename = os.path.join(config_dir, "config.json")
     if os.path.exists(config_filename):
       with open(config_filename, "r") as config_file:
@@ -49,11 +50,18 @@ class Settings(object):
     return config, aux_files
 
   def _write_config(self):
-    config_dir = appdirs.user_data_dir(Settings.APP_NAME)
+    config_dir = Settings._alloc_config_dir()
     with open(os.path.join(config_dir, "config.json"), "w") as config_file:
       json.dump(self._config, config_file)
 
   def _write_aux_files(self):
-    config_dir = appdirs.user_data_dir(Settings.APP_NAME)
+    config_dir = Settings._alloc_config_dir()
     with open(os.path.join(config_dir, "aux_files.json"), "w") as aux_files_file:
       json.dump(self._aux_files, aux_files_file)
+
+  @staticmethod
+  def _alloc_config_dir():
+    config_dir = appdirs.user_data_dir(appname=Settings.APP_NAME, appauthor=Settings.APP_AUTHOR)
+    if not os.path.exists(config_dir):
+      os.makedirs(config_dir)
+    return config_dir
--- zync.py
+++ zync.py
@@ -5,7 +5,7 @@ A Python wrapper around the Zync HTTP API.
 """
 
 
-__version__ = '1.4'
+__version__ = '1.4.1'
 
 
 import argparse
